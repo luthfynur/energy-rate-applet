292770ec610489db948c591127c48cb57cee86f5
Luthfy Nur <nurluthfy98@gmail.com>
2026-02-17 11:02:15 +0700

    initial commit

---
+++ src/app.rs
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: MPL-2.0
+
+use std::process::Command;
+use std::time::Duration;
+use crate::config::Config;
+use cosmic::cosmic_config::{self, CosmicConfigEntry};
+use cosmic::iced::{window::Id, Subscription};
+use cosmic::prelude::*;
+use cosmic::iced;
+use futures_util::SinkExt;
+
+/// The application model stores app-specific state used to describe its interface and
+/// drive its logic.
+#[derive(Default)]
+pub struct AppModel {
+    /// Application state which is managed by the COSMIC runtime.
+    core: cosmic::Core,
+    /// The popup id.
+    popup: Option<Id>,
+    /// Configuration data that persists between application runs.
+    config: Config,
+    /// Example row toggler.
+    example_row: bool,
+    energy_rate: String,
+}
+
+/// Messages emitted by the application and its widgets.
+#[derive(Debug, Clone)]
+pub enum Message {
+    PopupClosed(Id),
+    SubscriptionChannel,
+    UpdateConfig(Config),
+    UpdateEnergyRate,
+}
+
+/// Create a COSMIC application from the app model
+impl cosmic::Application for AppModel {
+    /// The async executor that will be used to run your application's commands.
+    type Executor = cosmic::executor::Default;
+
+    /// Data that your application receives to its init method.
+    type Flags = ();
+
+    /// Messages which the application and its widgets will emit.
+    type Message = Message;
+
+    /// Unique identifier in RDNN (reverse domain name notation) format.
+    const APP_ID: &'static str = "com.github.luthfynur.energy-rate-applet";
+
+    fn core(&self) -> &cosmic::Core {
+        &self.core
+    }
+
+    fn core_mut(&mut self) -> &mut cosmic::Core {
+        &mut self.core
+    }
+
+    /// Initializes the application with any given flags and startup commands.
+    fn init(
+        core: cosmic::Core,
+        _flags: Self::Flags,
+    ) -> (Self, Task<cosmic::Action<Self::Message>>) {
+        // Construct the app model with the runtime's core.
+        let app = AppModel {
+            core,
+            config: cosmic_config::Config::new(Self::APP_ID, Config::VERSION)
+                .map(|context| Config::get_entry(&context).unwrap_or_else(|(_errors, config)| {
+                    // for why in errors {
+                    //     tracing::error!(%why, "error loading app config");
+                    // }
+
+                    config
+                }))
+                .unwrap_or_default(),
+            energy_rate: get_energy_rate(),
+            ..Default::default()
+        };
+
+        (app, Task::none())
+    }
+
+    fn on_close_requested(&self, id: Id) -> Option<Message> {
+        Some(Message::PopupClosed(id))
+    }
+
+    /// Register subscriptions for this application.
+    ///
+    /// Subscriptions are long-lived async tasks running in the background which
+    /// emit messages to the application through a channel. They may be conditionally
+    /// activated by selectively appending to the subscription batch, and will
+    /// continue to execute for the duration that they remain in the batch.
+    fn subscription(&self) -> Subscription<Self::Message> {
+        struct MySubscription;
+
+        Subscription::batch(vec![
+            // Create a subscription which emits updates through a channel.
+            Subscription::run_with_id(
+                std::any::TypeId::of::<MySubscription>(),
+                cosmic::iced::stream::channel(4, move |mut channel| async move {
+                    _ = channel.send(Message::SubscriptionChannel).await;
+
+                    futures_util::future::pending().await
+                }),
+            ),
+            // Watch for application configuration changes.
+            self.core()
+                .watch_config::<Config>(Self::APP_ID)
+                .map(|update| {
+                    // for why in update.errors {
+                    //     tracing::error!(?why, "app config error");
+                    // }
+
+                    Message::UpdateConfig(update.config)
+                }),
+            iced::time::every(Duration::from_secs(1)).map(|_| Message::UpdateEnergyRate),
+        ])
+    }
+
+    /// Handles messages emitted by the application and its widgets.
+    ///
+    /// Tasks may be returned for asynchronous execution of code in the background
+    /// on the application's async runtime. The application will not exit until all
+    /// tasks are finished.
+    fn update(&mut self, message: Self::Message) -> Task<cosmic::Action<Self::Message>> {
+        match message {
+            Message::SubscriptionChannel => {
+                // For example purposes only.
+            }
+            Message::UpdateConfig(config) => {
+                self.config = config;
+            }
+            Message::PopupClosed(id) => {
+                if self.popup.as_ref() == Some(&id) {
+                    self.popup = None;
+                }
+            },
+            Message::UpdateEnergyRate => {
+                let new_energy_rate = get_energy_rate();
+                self.energy_rate = new_energy_rate;
+            }
+        }
+        Task::none()
+    }
+
+    /// Describes the interface based on the current state of the application model.
+    ///
+    /// The applet's button in the panel will be drawn using the main view method.
+    /// This view should emit messages to toggle the applet's popup window, which will
+    /// be drawn using the `view_window` method.
+    fn view(&self) -> Element<'_, Self::Message> {
+        let text = self.core.applet.text(&self.energy_rate);
+        let padding = cosmic::iced::Padding { top: 0.0 , right: 10.0, bottom: 0.0, left: 10.0 };
+        let container: cosmic::widget::Container<Message, Theme>  = cosmic::widget::container(text).padding(padding);
+        self.core.applet.autosize_window(container).into()
+    }
+
+    fn style(&self) -> Option<cosmic::iced_runtime::Appearance> {
+        Some(cosmic::applet::style())
+    }
+}
+
+fn get_energy_rate() -> String {
+    let output = Command::new("upower")
+        .arg("-i")
+        .arg("/org/freedesktop/UPower/devices/battery_BAT0")
+        .output()
+        .expect("Failed to execute upower");
+
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        for line in stdout.lines() {
+            if line.trim_start().starts_with("energy-rate:") {
+                eprintln!("{}", line.replace("energy-rate:", "").trim());
+                return format!("Energy Rate: {}",line.replace("energy-rate:", "").trim());
+            }
+        }
+    } else {
+        eprintln!("Error: {}", String::from_utf8_lossy(&output.stderr));
+        return "Error".into();
+    }
+
+    "".into()
+}
